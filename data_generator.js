2c79ba124339ee41deeee24ce5c6948ebfc2f4b6
keithGrout <keiwi.g@gmail.com>
2014-01-14 12:59:31 -0800

    first commit

--- data_generator.js
+++ data_generator.js
@@ -1,23 +1,40 @@
 /*
  * NOTE: This file generates fake tweet data, and is not intended to be part of your implementation.
  * You can safely leave this file untouched, and confine your changes to index.html.
+ 
+Creates two global variables, users and streams. 
+
+global users is simply a list of all the properties in stream.users
+users is an array of strings -- all the usernames that you're following.
+streams is an object with two properties, users and home.
+streams.home is an array of all tweets from all the users you're following.
+streams.users is an object with properties for each user. streams.users.shawndrost has all of shawndrost's tweets.
+Kicks off a periodic process that puts more data in streams.
+You'll mostly be working in the javascript block of index.html. Note: The generated tweets will be displayed in reverse chronological order.
  */
 
 // set up data structures
-window.streams = {};
-streams.home = [];
-streams.users = {};
-streams.users.shawndrost = [];
-streams.users.sharksforcheap = [];
-streams.users.mracus = [];
+window.streams = {};                                         // global object stream
+streams.home = [];											 // streams has 2 properties, one is an array the other is an object
+streams.users = {};                                          // home is an array and users is an object
+streams.users.shawndrost = [];                               // the users object inside stream has four properties, each a username. 
+streams.users.sharksforcheap = [];                           // each username inside stream.users is an array.
+streams.users.mracus = [];              
 streams.users.douglascalhoun = [];
-window.users = Object.keys(streams.users);
+window.users = Object.keys(streams.users);                   // global object users is an array with all the properties of streams.users 
+                                                             // i.e., the names of each user as a string
+                                                             // as a side note, Object.keys method returns all the properties of an object as strings
 
 // utility function for adding tweets to our data structures
-var addTweet = function(newTweet){
-  var username = newTweet.user;
-  streams.users[username].push(newTweet);
-  streams.home.push(newTweet);
+var addTweet = function(newTweet){                           // addTweet takes a newTweet parameter, an object, defined in generateRandomTweet()
+  var username = newTweet.user;                              // the object has a user property, a message property, and createdAt property
+  streams.users[username].push(newTweet);                    // all randomly generated in the generateRandomTweet function.
+  streams.home.push(newTweet);                               // addTweet takes the user property and stores it in username
+          													 // using username, it adds the tweet to the array of individual user tweets
+          													 // it also adds it to the list of all tweets from everyone
+          													 
+          													 // tl:dr? addTweet takes a tweet object and adds it to the user array of tweets
+          													 // and the array of all tweets in general
 };
 
 // utility function
@@ -37,24 +54,27 @@ var randomMessage = function(){
   return [randomElement(opening), randomElement(verbs), randomElement(objects), randomElement(nouns), randomElement(tags)].join(' ');
 };
 
+
+
 // generate random tweets on a random schedule
 var generateRandomTweet = function(){
-  var tweet = {};
-  tweet.user = randomElement(users);
-  tweet.message = randomMessage();
-  tweet.created_at = new Date();
-  addTweet(tweet);
+  var tweet = {};									// a tweet is randomly generated with three properties, then sent to the addTweet function
+  tweet.user = randomElement(users); 				// It has a random 'user' property from the array of all users
+  tweet.message = randomMessage();					// It has a random 'message' property from randomMessage()
+  tweet.created_at = new Date();					// It has a timestamp
+  addTweet(tweet);                                  // Then, the newly created tweet object is passed into addTweet()
 };
 
-for(var i = 0; i < 10; i++){
-  generateRandomTweet();
+for(var i = 0; i < 10; i++){                        // running generateRandomTweet ten times creates 10 tweet objects
+  generateRandomTweet();                            // and all ten are passed to addTweet, which adds each tweet to the
+  													// users tweets, and all tweets arrays.
 }
 
-var scheduleNextTweet = function(){
-  generateRandomTweet();
-  setTimeout(scheduleNextTweet, Math.random() * 1500);
+var scheduleNextTweet = function(){                       // a random tweet is generated and added to the user tweets and all tweets arrays
+  generateRandomTweet();								  // this function calls itself forever (recursion with no base case)
+  setTimeout(scheduleNextTweet, Math.random() * 1500);    // setTimeout calls scheduleNextTweet randomly between 0 and 1.5 seconds non-inclusive
 };
-scheduleNextTweet();
+scheduleNextTweet();    								  // jumpin' off point for the whole shebang		
 
 // utility function for letting students add "write a tweet" functionality
 // (note: not used by the rest of this file.)
